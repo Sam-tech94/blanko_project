464607ad4e3027a8bfaf4ee4e7b761dda604270e
aramya <arman1993.samvel@gmail.com>
2022-09-02 21:57:23 +0400

    added new functions

--- index.js
+++ index.js
@@ -28,56 +28,62 @@ let sliderImages = document.querySelectorAll(".slide"),
     arrowRight = document.querySelector("#arrow-right"),
     current = 0;
 
+    
 // clear all images
-function reset() {
-    for (let i = 0; i < sliderImages.length; i++) {
-        sliderImages[i].style.display = "none";
-    }
+const reset = () => {
+    sliderImages.forEach(slide => slide.style.display = "none");
 }    
 
-function showAll() {
+const showAll = () => {
     for (let i = 0; i < sliderImages.length; i++) {
         sliderImages[i].style.display = "block";
     }
+
 }
 
 //init slider
-function startSlide() {
+const startSlide = () => {
     reset();
     sliderImages[current].style.display = "block";
 }
 
 //show prev
-function slideLeft() {
+const slideLeft = () => {
     reset();
     sliderImages[current - 1].style.display = "block";
     current--;
 }
 
 // show next
-function slideRight() {
+const  slideRight = () => {
     reset();
     sliderImages[current + 1].style.display = "block";
     current++;
 }
 
-//left arrow click
-arrowLeft.addEventListener("click", () => {
+const moveSlideLeft = () => {
     if (current === 0) {
         current = sliderImages.length;
     }
 
     slideLeft();
-})
+};
 
-//right arrow click
-arrowRight.addEventListener("click", () => {
+//left arrow click
+arrowLeft.addEventListener("click", moveSlideLeft)
+
+const moveSlideRight = () => {
     if (current === sliderImages.length - 1) {
         current = -1;
     }
 
     slideRight();
-});
+};
+
+
+
+//right arrow click
+arrowRight.addEventListener("click", moveSlideRight);
 
 const sliding = () => window.innerWidth <= 900 ? startSlide() : showAll();
 
@@ -85,6 +91,18 @@ window.addEventListener('resize', sliding, true);
 
 sliding()
 
+
+// popup sign-in
+
+document.getElementById("sign_in_active").addEventListener("click", () => {
+    document.querySelector(".div_form").classList.add("active")
+    document.body.style.opacity = "0.9"
+});
+
+document.querySelector(".div_form .close_btn").addEventListener("click", () => {
+    document.querySelector(".div_form").classList.remove("active")
+    document.body.style.opacity = "1";
+})
 
 
 
